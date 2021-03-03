c8a7c56ce6f01b6bcc31847bd2bc1b5244ef99d4
Sam Scherf <csmoran@protonmail.com>
2021-03-03 00:45:40 -0500

    Initial Cut Implantation
    The code isn't very clean but it's proof of concept that a cut function
    works quickly.

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,41 @@ fn main() {
     let matches = app.get_matches();
 
     // Get sub command matches
+    if let Some(scale_matches) = matches.subcommand_matches("cut") {
+            
+        // Get parameter values
+        let image_path = scale_matches.value_of("input").unwrap();
+        let output = scale_matches.value_of("output").unwrap();
+
+        // Open Image
+        let mut orginal_image = image::open(image_path).unwrap();
+
+        // Set size
+        let size = 64;
+
+        // Find position of /
+        let break_postion = image_path.rfind("/").unwrap();
+
+        // Get image name
+        let image_name = &image_path[break_postion..image_path.chars().count()];
+
+        // Get dimensions
+        let (orginal_width, orginal_height) = orginal_image.dimensions();
+
+        // Get images
+        let images_wide = orginal_width / size;
+        let images_tall = orginal_height / size;
+
+        // Loop though images
+        for x in 0..images_wide {
+            for y in 0..images_tall {
+                let cropped = orginal_image.crop_imm(x*size, y*size, size, size);
+                cropped.save(output.to_string() + "r" + "." + &x.to_string() + "." + &y.to_string() + ".png");
+            }
+        }
+    }
+
+    // Get sub command matches
     if let Some(scale_matches) = matches.subcommand_matches("scale") {
             
         // Get parameter values
